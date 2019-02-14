f3d2a121406238bda372cf9771871ab49d0cd9fb
gogogohuang <gogogohuang@gmail.com>
2019-02-14 23:58:02 +0800

    update animation

--- components/content/Home.js
+++ components/content/Home.js
@@ -18,11 +18,11 @@ const HomeContainer = styled(Flex)`
   background-size: cover;
   background-image: url('https://dearparanoid.github.io/jinze/img/home.jpg');
   background-position: center;
-`;;
+  animation: ${fadeIn} 2s linear;
+`;
 
 const Title = styled.h1`
   color: #4e4e4e;
-  animation: ${fadeIn} 2s linear;
   ${customMedia.lessThan('tablet')`
     font-size: 5em;
     text-align: center;
