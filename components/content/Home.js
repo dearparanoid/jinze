1eac35b29c902c5f151ad5fcbd93485a89a1bf84
gogogohuang <gogogohuang@gmail.com>
2019-02-14 22:21:53 +0800

    home done

--- components/content/Home.js
+++ components/content/Home.js
@@ -1,22 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import Flex from 'styled-flex-component';
+import { customMedia } from '../helper/Media';
 
-// import home from 'home.jpg';
-
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
+const fadeIn = keyframes`
+  from {
+    opacity:0;
   }
-  render() {
-    return (
-      <div className="homeTitle">
-        {/* <img src={home} alt="" className="homeImg" /> */}
-        <h1>Light Always</h1>
-      </div>
-    );
+  to {
+    opacity:1;
   }
-}
+`;
+
+const HomeContainer = styled(Flex)`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-image: url('./img/home.jpg');
+  background-position: center;
+`;;
+
+const Title = styled.h1`
+  color: #4e4e4e;
+  animation: ${fadeIn} 2s linear;
+  ${customMedia.lessThan('tablet')`
+    font-size: 5em;
+  `}
+
+  ${customMedia.greaterThan('tablet')`
+    font-size: 10em;
+  `}
+`;
+
+const Home = () => (
+  <HomeContainer center full id="">
+    <Flex full center><Title>Light Always</Title></Flex>
+  </HomeContainer>
+)
 
 export default Home;
--- components/menu/MenuBM.js
+++ components/menu/MenuBM.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { slide as Menu } from 'react-burger-menu';
+import { slide as Menu } from 'react-burger-menu'; 
 
-import Home from 'content/Home';
-import About from 'content/About';
-import Resume from 'content/Resume';
+import Home from '../content/Home';
+import About from '../content/About';
+import Resume from '../content/Resume';
 // import PortfolioSet from 'content/portfolio/PortfolioSet';
 import GetPhotosetContainer from '../../components/container/GetPhotosetContainer';
 
@@ -13,18 +13,12 @@ import 'menu.scss';
 class MenuBM extends Component {
   constructor(props) {
     super(props);
-
-    this.handleMenu = this.handleMenu.bind(this);
-
     this.state = {
       close: false,
     };
   }
-  shouldComponentUpdate() {
-    return true;
-  }
 
-  handleMenu() {
+  handleMenu = () => {
     this.setState({ close: false });
   }
 
@@ -33,12 +27,12 @@ class MenuBM extends Component {
       <Router>
         <div>
           <Menu noOverlay={false} isOpen={this.state.close}>
-            <Link className="bm-menu-item" to="/jinze">Home</Link>
+            <Link className="bm-menu-item" to="/" onClick={this.handleMenu}>Home</Link>
             <Link className="bm-menu-item" to="/about" onClick={this.handleMenu}>About</Link>
             <Link className="bm-menu-item" to="/resume" onClick={this.handleMenu}>Resume</Link>
             <Link className="bm-menu-item" to="/portfolio" onClick={this.handleMenu} style={{ width: '70%', display: 'initial' }}>Portfolio</Link>
           </Menu>
-          <Route exact={true} path="/jinze" component={Home} />
+          <Route exact={true} path="/" component={Home} />
           <Route path="/about" component={About} />
           <Route path="/resume" component={Resume} />
           <Route path="/portfolio" component={GetPhotosetContainer} />
