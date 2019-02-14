7fd389b1ff5a39baea58b1d787c85254c9063c66
gogogohuang <gogogohuang@gmail.com>
2019-02-15 00:56:30 +0800

    WIP : about`

--- components/content/About.js
+++ components/content/About.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import FaInstagram from 'react-icons/lib/fa/instagram';
 import FaFlickr from 'react-icons/lib/fa/flickr';
 import FaWordpress from 'react-icons/lib/fa/wordpress';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import Flex from 'styled-flex-component';
-import { customMedia } from '../helper/Media';
+import { customMedia, fadeIn } from '../helper/Media';
 
 import about from 'about.jpg';
 import 'about.scss';
@@ -14,10 +14,30 @@ const ABOUT = ['Front-end', 'Photo', 'Surfing', 'Hiphop', 'Diving']
 const Container = styled(Flex)`
   width: 100vw;
   height: 100vh;
+`;
+
+const TextContainer = styled.div`
+  position: absolute;
+  top: 20%;
+  display: none;
+  font-size: 1.5em;
+  font-weight: bold;
+  color: lightgray;
+  text-shadow: 1px 1px 1em lightgray;
+`;
+
+const ImgContainer = styled(Flex)`
+  &:hover > ${TextContainer} {
+    display: block;
+    animation: ${fadeIn} 1s linear;
+  }
   img {
-    margin: 25px auto 0px;
+    display: block;
+    position: relative;
     width: 50vw;
+    height: auto;
     padding-top: 0;
+    cursor: pointer;
   }
 `;
 
@@ -36,10 +56,7 @@ const SocialLink = styled.a`
   text-decoration: none;
 `;
 
-const TextContainer = styled.div`
-  font-size: 1.5em;
-  font-weight: bold;
-`;
+
 
 const renderAboutText = () => (
   <TextContainer>
@@ -49,13 +66,15 @@ const renderAboutText = () => (
 
 const About = (props) => (
   <Container column center>
-    <img src={about} alt="" />
+    <ImgContainer column center>
+      <img src={about} alt="" />
+      {renderAboutText()}
+    </ImgContainer>
     <SocialLinkContainer justifyAround alignCenter>
       <SocialLink href="https://www.instagram.com/gogogohuang" target="_blank" rel="noopener noreferrer"><FaInstagram size={28} /></SocialLink>
       <SocialLink href="https://www.flickr.com/gogogohuang" target="_blank" rel="noopener noreferrer"><FaFlickr size={28} /></SocialLink>
       <SocialLink href="https://jzhuangblog.wordpress.com/" target="_blank" rel="noopener noreferrer"><FaWordpress size={28} /></SocialLink>
     </SocialLinkContainer>
-    {renderAboutText()}
   </Container>
 )
 
